7c024fc8930b38ad046bd16fe8678601be227ace
Darya Pleshkunova <d.pleshkunova@yandex.ru>
2024-03-29 13:29:04 +0300

    fixes

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -8,8 +8,7 @@ enum METHODS {
 function queryStringify(data: Record<string, any>): string {
   const searchParams = new URLSearchParams(data);
   const queryString = searchParams.toString();
-  const adaptedString = queryString.replaceAll('%5B', '[').replaceAll('%5D', ']').replaceAll('%2C', ',').replaceAll('+', ' ');
-
+  const adaptedString = queryString.replace(/%5B/g, '[').replace(/%5D/g, ']').replace(/%2C/g, ',').replace(/\+/g, ' ');
   return adaptedString;
 }
 
