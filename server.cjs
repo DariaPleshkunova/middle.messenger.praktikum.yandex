4d304119e3571c422e444d4e9cf27c412c1d8a39
Darya Pleshkunova <d.pleshkunova@yandex.ru>
2024-04-27 16:20:27 +0300

    express routing

--- server.cjs
+++ server.cjs
@@ -7,6 +7,18 @@ const dirPath = `${__dirname}/dist`;
 
 app.use(express.static(dirPath));
 
+app.get('/login', (req, res) => {
+  res.sendFile(`${dirPath}/index.html`);
+})
+
+app.get('/sign-up', (req, res) => {
+  res.sendFile(`${dirPath}/index.html`);
+})
+
+app.get('/messenger', (req, res) => {
+  res.sendFile(`${dirPath}/index.html`);
+})
+
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
